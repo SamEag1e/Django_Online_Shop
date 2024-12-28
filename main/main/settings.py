96ad16cdb2ec416c161e36892e9878b8e450b618
SamEagle <samadeagle@yahoo.com>
2024-12-28 05:12:43 +0330

    Add middleware for admin_site login_required for all urls except for login itself.

--- main/main/settings.py
+++ main/main/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "admin_panel.middleware.AdminPanelMiddleware",
 ]
 
 ROOT_URLCONF = "main.urls"
