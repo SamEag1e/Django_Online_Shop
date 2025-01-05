7e51ce6472140495727a1e5adf8d689fea279bca
SamEagle <samadeagle@yahoo.com>
2025-01-05 16:24:00 +0330

    Remove unused accounts.middleware and accounts.templatetags.

--- main/main/settings.py
+++ main/main/settings.py
@@ -57,7 +57,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "accounts.middleware.CustomerLoginRequiredMiddleware",
     "admin_panel.middleware.AdminLoginRequiredMiddleware",
 ]
 
