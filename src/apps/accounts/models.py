184f3f3c92f9e0f70c6e0b4a4b1d8f76bc68850d
SamEagle <samadeagle@yahoo.com>
2025-04-22 02:13:02 +0330

    Rename bank_carts module to bank_cart_model.

--- src/apps/accounts/models.py
+++ src/apps/accounts/models.py
@@ -1,3 +1,3 @@
 from .model_entities.custom_user_model import CustomUser
 from .model_entities.address_model import Address
-from .model_entities.bank_carts import BankCart
+from .model_entities.bank_cart_model import BankCart
